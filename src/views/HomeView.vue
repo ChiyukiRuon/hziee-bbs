6e2b85701266887269fbbebd89b7f6da8aab4853
ChiyukiRuon <1275846107@qq.com>
2023-03-01 18:09:07 +0800

    首次提交

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <el-tabs :stretch="true">
+            <el-tab-pane label="推荐">
+                <span v-for="item in items" :key="item">
+                    <PostCardView username="迟雨_Official" uid="Chiyu_official" :certified="false" :post="item" />
+                </span>
+            </el-tab-pane>
+            <el-tab-pane label="你的关注">
+                <span v-for="item in items" :key="item">
+                    <PostCardView avatar-src="https://static.chiyukiruon.top/images/touxiang_01.GIF" username="千雪琉音" uid="ChiyukiRuon" :certified="true" :post="item" />
+                </span>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PostCardView from "@/views/PostCardView.vue";
+import { ref } from "vue";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+    name: "HomeView",
+    components: { PostCardView },
+    data() {
+        return {
+            items: ref(10)
+        }
+    }
 };
 </script>
+
+<style scoped>
+.home {
+    text-align: center;
+}
+</style>
